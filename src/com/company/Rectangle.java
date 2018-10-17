badab64433ebeb09b7ef9377b745ad44eff07c64
jacksonnerdcamp <jacksonjjacobs@gmail.com>
2018-10-17 08:42:52 -0400

    finishing up rectangle

--- src/com/company/Rectangle.java
+++ src/com/company/Rectangle.java
@@ -2,10 +2,25 @@ package com.company;
 
 public class Rectangle
 {
+    //fields
     private int height;
     private int width;
-    public Rectangle(height, width)
-    Rectangle.getArea()
-    Rectangle.getPerimeter()
+    //constructors
+    public Rectangle(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+    //methods
+    public double getArea(width, height)
+    {
+        int area = width * height;
+        return area;
+    }
+    public double getPerimeter(width, height)
+    {
+        int perimeter = 2 * width + 2 * height;
+        return perimeter;
+    }
 
 }
