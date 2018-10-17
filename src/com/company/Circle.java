e346bd44e90715c65d4dcb4252e6ff241c50accb
BT_1N3_27 <kfang9357@bths.edu>
2018-10-17 08:27:12 -0400

    circle class filled

--- src/com/company/Circle.java
+++ src/com/company/Circle.java
@@ -1,9 +1,26 @@
 package com.company;
 
 public class Circle {
+
     ///fields///
     private int radius;
+
     ///constructors///
+    public Circle(int radius)
+    {
+        this.radius = radius;
+    }
 
     ///methods///
+    public int getArea(int radius)
+    {
+        int area = (int) (3.14*radius*radius);
+        return area;
+    }
+
+    public int getPerimeter(int radius)
+    {
+        int perimeter = (int) (3.14*2*radius);
+        return perimeter;
+    }
 }
